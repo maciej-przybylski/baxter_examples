75e1f19faf5fcc223d26530542ddbea949d4a6df
Maciej Przybylski <maciej.przybylski@pw.edu.pl>
2022-03-04 13:03:26 -0500

    joint_velocity_wobbler bugfix

--- scripts/joint_velocity_wobbler.py
+++ scripts/joint_velocity_wobbler.py
@@ -57,7 +57,7 @@ class Wobbler(object):
         self._right_joint_names = self._right_arm.joint_names()
 
         # control parameters
-        self._rate = 500.0  # Hz
+        self._rate = 500  # Hz
 
         print("Getting robot state... ")
         self._rs = baxter_interface.RobotEnable(CHECK_VERSION)
